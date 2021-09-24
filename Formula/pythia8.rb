66fd957cac8f7931fa507ffe0e1a877642247f91
Paul Gessinger <hello@paulgessinger.com>
2021-09-24 14:37:50 +0000

    pythia8: add 8.243 bottle.

--- Formula/pythia8.rb
+++ Formula/pythia8.rb
@@ -6,6 +6,12 @@ class Pythia8 < Formula
   version "8.243"
   sha256 "067219fb737bfd379ddb602b609ff5cd04f0569782f8d9d59e403264fd8a21f3"
 
+  bottle do
+    root_url "https://github.com/acts-project/homebrew-tap/releases/download/pythia8-8.243"
+    sha256 catalina:     "f388a79a6d6505eb9e6c650c490380f77ae7e6337a3b767f8cba75073a4d2894"
+    sha256 x86_64_linux: "d3e604cb89762db6b5a70e736a549e0ac753b9d0aa43d607f8ed54de9286d844"
+  end
+
   def install
     args = %W[
       --prefix=#{prefix}
