08109606f8e8fa6f60f27cd3404d780bd391dfee
Paul Gessinger <hello@paulgessinger.com>
2021-09-24 14:38:51 +0000

    hepmc3: add 3.2.2 bottle.

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -5,6 +5,12 @@ class Hepmc3 < Formula
   url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.2.tar.gz"
   sha256 "0e8cb4f78f804e38f7d29875db66f65e4c77896749d723548cc70fb7965e2d41"
 
+  bottle do
+    root_url "https://github.com/acts-project/homebrew-tap/releases/download/hepmc3-3.2.2"
+    sha256                               catalina:     "900c856fc3ea469e2577d36fcae15e38e9e3be465ecf02318e80dcfb0e31ac8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "09914df83c39f7a4605f397c4bae55ba0218106355a7b25c36d727d8b87df023"
+  end
+
   option "with-test", "Test during installation"
   option "with-python", "Enable building of python bindings"
   option "with-root", "Enable root IO"
